5135f08d564f69cf79eac68c41ecceca4dcf7c06
signyrut <126098558+signyrut@users.noreply.github.com>
2023-04-21 11:22:44 +0200

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -14,8 +14,8 @@ application = Flask(__name__)
 # application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
 # application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
 
-### Code computer
-# application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
+### Code compute
+application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
 DBVAR="postgresql://uiixhwbxccodbt:e6db16a63e8f9dc609b3698fa7610b85e2cb2692850a2f5adc73063567345cbe@ec2-34-250-252-161.eu-west-1.compute.amazonaws.com:5432/dderaf94m6u3b5"
 application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
 application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
